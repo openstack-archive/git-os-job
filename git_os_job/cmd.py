f66d479ccf9583012b52138feac6c99ef7ab470f
Doug Hellmann <doug.hellmann@gmail.com>
2013-09-11 18:52:27 -0400

    first version with basic argument processing

---
+++ git_os_job/cmd.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import argparse
+import subprocess
+import sys
+import webbrowser
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Show the OpenStack job logs for a commit in a browser',
+    )
+    parser.add_argument(
+        '--base',
+        action='store',
+        default='http://logs.openstack.org',
+        help='base URL, defaults to %(default)s',
+    )
+    parser.add_argument(
+        '--url',
+        action='store_true',
+        default=False,
+        help='show the URL but do not open it',
+    )
+    parser.add_argument(
+        'ref',
+        nargs='?',
+        default='HEAD',
+        help='the git reference, tag, or commit, defaults to %(default)s',
+    )
+    args = parser.parse_args()
+
+    ref = args.ref
+    import os
+    try:
+        ref_hash = subprocess.check_output(
+            ['git', 'show-ref', '-s', ref]
+        ).rstrip()
+    except subprocess.CalledProcessError:
+        # Maybe they gave us a commit id
+        try:
+            ref_hash = subprocess.check_output(
+                ['git', 'show', '--pretty=format:%H', '--quiet', ref]
+            ).rstrip()
+        except subprocess.CalledProcessError:
+            sys.stderr.write('Could not get hash for ref %r\n' % ref)
+            return 1
+
+    url = '%s/%s/%s/' % (args.base, ref_hash[:2], ref_hash)
+    if args.url:
+        print(url)
+    else:
+        webbrowser.open(url)
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
