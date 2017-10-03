3077d6dca43292095116da9bf037550be2ae4690
Doug Hellmann <doug@doughellmann.com>
2017-10-03 13:02:48 -0400

    don't decode bytes to unicode twice
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- git_os_job/cmd.py
+++ git_os_job/cmd.py
@@ -95,8 +95,7 @@ def main():
                 sys.stderr.write('Could not get hash for ref %r\n' % ref)
                 return 1
 
-        ref_hash_str = ref_hash.decode('utf8')
-        url = '%s/%s/%s/' % (args.base, ref_hash_str[:2], ref_hash_str)
+        url = '%s/%s/%s/' % (args.base, ref_hash[:2], ref_hash)
 
     if args.url:
         print(url)
